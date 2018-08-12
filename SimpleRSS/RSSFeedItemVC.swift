f175679d4aac812c32a680c06bbf30668f7778ba
mubashir <mubashir2k17@gmail.com>
2018-08-12 12:40:46 +0500

    added a new vc

---
+++ SimpleRSS/RSSFeedItemVC.swift
@@ -0,0 +1,41 @@
+//
+//  RSSFeedItemVC.swift
+//  SimpleRSS
+//
+//  Created by Tintash on 12/08/2018.
+//  Copyright © 2018 AsadJamil. All rights reserved.
+//
+
+import UIKit
+import FeedKit
+
+class RSSFeedItemVC: UIViewController {
+
+    var rssFeedItem: RSSFeedItem? = nil
+
+    @IBOutlet weak var titleLbl: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLbl.text = rssFeedItem?.title
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SimpleRSS/SimpleRSSVC.swift
+++ SimpleRSS/SimpleRSSVC.swift
@@ -7,27 +7,51 @@
 //
 
 import UIKit
+import FeedKit
 
 class SimpleRSSVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
-
+    var rssFeed: RSSFeed? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let feedURL = URL(string: "http://images.apple.com/main/rss/hotnews/hotnews.rss")!
+        let parser = FeedParser(URL: feedURL)
+        parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { (result) in
+            // Do your thing, then back to the Main thread
+            DispatchQueue.main.async {
+                // ..and update the UI
+                self.rssFeed = result.rssFeed
+                self.tableView.reloadData()
+            }
+        }
     }
 }
 
 extension SimpleRSSVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return rssFeed?.items?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if let feed = rssFeed {
+            cell.textLabel?.text = feed.items?[indexPath.row].title
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sb = UIStoryboard.init(name: "Main", bundle: nil)
+        if let vc = sb.instantiateViewController(withIdentifier: "RSSFeedItemVC") as? RSSFeedItemVC {
+            if let feed = rssFeed {
+                vc.rssFeedItem = feed.items?[indexPath.row]
+            }
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
